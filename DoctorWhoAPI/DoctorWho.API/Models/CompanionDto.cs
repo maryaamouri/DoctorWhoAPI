6cd0a42258ac8c5cb6e56551063acbf4e770adae
Marya_Amouri <marya.ammori@gmail.com>
2023-06-28 18:46:37 +0300

    add companion dto

---
+++ DoctorWhoAPI/DoctorWho.API/Models/CompanionDto.cs
@@ -0,0 +1,16 @@
+﻿namespace DoctorWho.API.Models
+{
+
+        public class CompanionDto
+        {
+            public CompanionDto()
+            {
+                Episodes = new List<EpisodeDto>();
+            }
+            public int CompanionId { set; get; }
+            public string Name { set; get; }
+            public string WhoPlayed { set; get; }
+            public IList<EpisodeDto> Episodes;
+        }
+    }
+
