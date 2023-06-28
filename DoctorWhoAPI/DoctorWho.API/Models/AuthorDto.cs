660aaff4364c210584c4cccd3216bb6b375d5f45
Marya_Amouri <marya.ammori@gmail.com>
2023-06-28 18:44:53 +0300

    add AuthorDto.cs

---
+++ DoctorWhoAPI/DoctorWho.API/Models/AuthorDto.cs
@@ -0,0 +1,15 @@
+﻿namespace DoctorWho.API.Models
+{
+    public class AuthorDto
+    {
+        public AuthorDto()
+        {
+            Episodes = new List<EpisodeDto>();
+        }
+        public int AuthorId { set; get; }
+        public string Name { set; get; }
+
+        public IList<EpisodeDto> Episodes;
+    }
+}
+}
