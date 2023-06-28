ab8e377fe65858bc2ed5a3d0535aa2a47a39847f
Marya_Amouri <marya.ammori@gmail.com>
2023-06-28 18:45:52 +0300

    Add enemy dto

---
+++ DoctorWhoAPI/DoctorWho.API/Models/EnemyDto.cs
@@ -0,0 +1,15 @@
+﻿namespace DoctorWho.API.Models
+{
+    public class EnemyDto
+    {
+        public EnemyDto()
+        {
+            Episodes = new List<EpisodeDto>();
+        }
+        public int EnemyId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public IList<EpisodeDto> Episodes;
+    }
+}
+}
