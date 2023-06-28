ea10f50e1180ce13286417827581902bd8b914e4
Marya_Amouri <marya.ammori@gmail.com>
2023-06-28 18:29:55 +0300

    add DoctorDto.cs

---
+++ DoctorWhoAPI/DoctorWho.API/Models/DoctorDto.cs
@@ -0,0 +1,13 @@
+﻿namespace DoctorWho.API.Models
+{
+    public class DoctorDto
+    {
+        public int DoctorId { get; set; }
+        public string Number { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public DateTime FirstEpisodeDate { get; set; }
+        public DateTime LastEpisodeDate { get; set; }
+
+    }
+}
