fad93fc8abb9cab09f5e787b6a22a2359bb71c08
Marya_Amouri <marya.ammori@gmail.com>
2023-07-02 11:47:47 +0300

    move DB models to Entities foldre

---
+++ DoctorWhoAPI/DoctorWhoDomain/Entities/Companion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoctorWhoDomain.Entities
+{
+    public class Companion
+    {
+        public Companion()
+        {
+            Episodes = new List<Episode>();
+        }
+        public int CompanionId { set; get; }
+        public string Name { set; get; }
+        public string WhoPlayed { set; get; }
+        public IList<Episode> Episodes;
+    }
+}
