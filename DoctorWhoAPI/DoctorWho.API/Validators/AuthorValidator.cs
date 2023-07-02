8930c34d9ad65e2a9178101bb7c4cdb6af4db9ce
Marya_Amouri <marya.ammori@gmail.com>
2023-07-02 13:32:35 +0300

    add validation for Author

---
+++ DoctorWhoAPI/DoctorWho.API/Validators/AuthorValidator.cs
@@ -0,0 +1,16 @@
+﻿using DoctorWho.API.Models;
+using FluentValidation;
+
+namespace DoctorWho.API.Validators
+{
+    public class AuthorValidator : AbstractValidator<AuthorDto>
+    {
+        public AuthorValidator()
+        {
+            RuleFor(author => author.Name)
+                .NotEmpty().WithMessage("Author Name is required")
+                .Matches("^[a-zA-Z ] *$").WithMessage("Author Name cannot contain special charactors")
+                .MaximumLength(50).WithMessage("Too long Name");
+        }
+    }
+}
