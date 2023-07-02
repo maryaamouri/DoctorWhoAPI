f3dea26d90825f3452a33c926bd374e3c5a57588
Marya_Amouri <marya.ammori@gmail.com>
2023-07-02 16:14:32 +0300

    add companion validator

---
+++ DoctorWhoAPI/DoctorWho.API/Validators/CompanionValidator.cs
@@ -0,0 +1,17 @@
+﻿using DoctorWhoDomain.Entities;
+using FluentValidation;
+
+public class CompanionValidator : AbstractValidator<Companion>
+{
+    public CompanionValidator()
+    {
+        RuleFor(c => c.Name)
+            .NotEmpty().WithMessage("Companion name is required.")
+            .MaximumLength(50).WithMessage("Companion name cannot exceed 50 characters.")
+            .Matches("^[a-zA-Z ] *$").WithMessage("Companion Name cannot include special charactors");
+
+        RuleFor(c => c.WhoPlayed)
+            .NotEmpty().WithMessage("Actor name is required.")
+            .MaximumLength(50).WithMessage("Actor Name cannot exceed 50 characters.");
+    }
+}
