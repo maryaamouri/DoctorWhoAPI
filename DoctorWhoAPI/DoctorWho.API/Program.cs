bbdca76570a3e10026108afc17f5f3cede294bef
Marya_Amouri <marya.ammori@gmail.com>
2023-07-02 11:56:08 +0300

    set up AutoMapper in Program.cs

--- DoctorWhoAPI/DoctorWho.API/Program.cs
+++ DoctorWhoAPI/DoctorWho.API/Program.cs
@@ -1,18 +1,32 @@
+using DoctorWhoData;
+using DoctorWhoData.Repos;
+using DoctorWhoDomain;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddControllers()
+                .AddJsonOptions(opt => opt.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 builder.Services.AddDbContext<DoctorWhoDBContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DoctorWhoConnection"))
            .EnableSensitiveDataLogging()
            .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking));
 
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddScoped<IDoctorRepo, DoctorRepo>();
+builder.Services.AddScoped<IAuthorRepo, AuthorRepo>();
+builder.Services.AddScoped<ICompanionRepo, CompanionRepo>();
+builder.Services.AddScoped<IEnemyRepo, EnemyRepo>();
+builder.Services.AddScoped<IEpisodeRepo, EpisodeRepo>();
 
 var app = builder.Build();
+app.MapControllers();
+
 
 app.UseHttpsRedirection();
+app.UseRouting();
 
-app.MapGet("/", () =>
-{
-       return "Hello API";
-})
-.WithOpenApi();
 app.Run();
