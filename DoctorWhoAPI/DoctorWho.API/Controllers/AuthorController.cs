94e7d5cd54c9d874c061837fa9ffb990c4a509f1
Marya_Amouri <marya.ammori@gmail.com>
2023-07-06 11:51:02 +0300

    add AuthorController with Update API

---
+++ DoctorWhoAPI/DoctorWho.API/Controllers/AuthorController.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using DoctorWho.API.Models;
+using DoctorWhoData.Repos;
+using DoctorWhoDomain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DoctorWho.API.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorController : ControllerBase
+    {
+        private readonly IAuthorRepo _authorRepo;
+        private readonly IMapper _mapper;
+
+        public AuthorController(IAuthorRepo authorRepo, IMapper mapper)
+        {
+            _authorRepo = authorRepo;
+            _mapper = mapper;
+        }
+        [HttpPut]
+        public async Task<ActionResult<AuthorDto>> Update(AuthorDto author)
+        {
+            var newauthor = await _authorRepo.UpdateAsync(_mapper.Map<Author>(author));
+            if(newauthor == null) {
+                return NotFound();
+            }
+            return Ok(newauthor);
+        }
+    }
+}
